8f341f5e3bd884845d593eb36e70527df863b206
kuba <jakub.wolanski@mobile-5.com>
2022-01-19 16:29:15 +0100

    fix seek

--- screen.py
+++ screen.py
@@ -51,8 +51,8 @@ while True:
         continue
     lcd.message(title.display, 1)
     # print(title.display)
-
-    times = (timeString(int((status['seek'] if status['seek'] is not None else 0) / 1000)) + ' ' + timeString(status['duration']) + ' ' + status['trackType'])[:16]
+    seek = status['seek'] if status['seek'] is not None else 0
+    times = (timeString(int(seek / 1000)) + ' ' + timeString(status['duration']) + ' ' + status['trackType'])[:16]
     # print(times)
     lcd.message(times, 2)
     title.tick(status['title'] + ' : ' + status['artist'])
